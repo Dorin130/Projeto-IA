c68a347bd627b2eafe2f4d2bb8bdf56ae4ef8ab0
Dorin130 <dorin130ytube@gmail.com>
2017-10-11 10:10:57 +0100

    first commit, added files

---
+++ SameGame.py
@@ -0,0 +1,131 @@
+# -*- coding: latin-1 -*-
+#-----------------------------------------------------------------------------------------------
+# Projeto-IA
+#	Inteligência Artificial
+#	1ª parte do Projecto
+#	IST @ 2017/2018 
+#
+#	Grupo 25
+#		83448 - Dorin Gujuman
+#		83504 - Manuel José Ribeiro Vidigueira
+#-----------------------------------------------------------------------------------------------
+#	  /$$$$$$                                     /$$$$$$                                   
+#	 /$$__  $$                                   /$$__  $$                                  
+#	| $$  \__/  /$$$$$$  /$$$$$$/$$$$   /$$$$$$ | $$  \__/  /$$$$$$  /$$$$$$/$$$$   /$$$$$$ 
+#	|  $$$$$$  |____  $$| $$_  $$_  $$ /$$__  $$| $$ /$$$$ |____  $$| $$_  $$_  $$ /$$__  $$
+#	 \____  $$  /$$$$$$$| $$ \ $$ \ $$| $$$$$$$$| $$|_  $$  /$$$$$$$| $$ \ $$ \ $$| $$$$$$$$
+#	 /$$  \ $$ /$$__  $$| $$ | $$ | $$| $$_____/| $$  \ $$ /$$__  $$| $$ | $$ | $$| $$_____/
+#	|  $$$$$$/|  $$$$$$$| $$ | $$ | $$|  $$$$$$$|  $$$$$$/|  $$$$$$$| $$ | $$ | $$|  $$$$$$$
+#	 \______/  \_______/|__/ |__/ |__/ \_______/ \______/  \_______/|__/ |__/ |__/ \_______/
+#-----------------------------------------------------------------------------------------------
+import utils
+#TEMP
+
+test = [[1,2,2,3,3],[2,2,2,1,3],[1,2,2,2,2],[1,1,1,1,1]]
+b1 = [[0,0,0,0,0],[0,2,3,3,0],[1,2,1,3,0],[2,2,2,2,0]] 
+large = [[1,3,2,1,2,1,2,2,1,2,2,1,1,3,1],[1,3,3,2,1,2,2,2,3,1,2,1,2,3,1],[1,1,1,2,3,2,3,3,2,2,3,1,1,3,1],[1,2,2,2,3,3,3,3,1,2,1,2,1,3,2],[1,3,1,3,2,2,2,2,3,1,1,2,3,2,1],[1,1,2,2,2,1,1,3,2,1,2,3,1,3,1],[3,1,3,2,2,2,3,3,3,1,3,3,2,1,1],[3,2,1,2,1,3,1,2,1,2,3,1,1,3,3],[2,3,1,2,3,3,1,2,3,3,3,2,1,1,1],[2,2,1,1,2,1,2,2,1,1,3,2,2,2,2]]
+
+#	TIPOS
+
+# TAI color
+# sem cor = 0
+# com cor > 0
+def get_no_color():
+	return 0
+def no_color (c):
+	return c==0
+def color (c):
+	return c > 0
+
+# TAI pos
+# Tuplo (l, c)
+def make_pos (l, c):
+	return (l, c)
+def pos_l (pos):
+	return pos[0]
+def pos_c (pos):
+	return pos[1]
+
+#Tipo group
+#O tipo group corresponde a uma lista de peças adjacentes com a mesma cor. A sua representação
+#interna é uma lista de elementos do tipo pos.
+
+def get_adjacent(board, pos):
+	""" retorna uma lista com elementos do tipo pos com elementos adjacentes da mesma cor indicada """
+	adj = []
+	l, c = pos_l(pos), pos_c(pos)
+
+	for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+		if 0 <= c + x < board_width(board) and 0 <= l + y < board_height(board):
+			test_pos = make_pos(l+y, c+x)
+			if board_get_color(board, pos) == board_get_color(board, test_pos):
+				adj.append(test_pos)
+	return adj
+
+def board_get_group(board, pos):
+	stack = set([pos])
+	group = set()
+
+	while len(stack) > 0:
+		curr = stack.pop()
+		for adjacent in get_adjacent(board, curr):
+			if adjacent not in group:
+				stack.add(adjacent)
+		group.add(curr)
+
+	return list(group)
+
+def board_find_groups(board):
+	"""devolve uma lista com os grupos de peças quese podem encontrar no tabuleiro, 
+	sendo um grupo de peças uma lista com uma ou mais peças."""
+
+	groups = []
+	visited = set()
+
+	for c in range(board_width(board)):
+		for l in range(board_height(board)):
+			curr = make_pos(l, c)
+			if curr not in visited:
+				curr_group = board_get_group(board, curr)
+				groups.append(curr_group)
+				for pos in curr_group:
+					visited.add(pos)
+	return groups
+
+
+def board_remove_group(board, group):
+	"""remove  o  grupo  do  tabuleiro fazendo
+	a  compactação  vertical  e  horizontal  das  peças."""
+	new_board = [x[:] for x in board] #copy the board
+	for pos in group:
+		board_del_color(new_board, pos)
+
+	new_board = list(map(list, zip(*new_board))) #this transposes the matrix
+	for row in new_board:
+		row.sort(key=lambda x: x != 0) #this pushes all zeroes to left
+
+	new_board.sort(key=lambda x: x.count(0) == board_height(board)) 
+
+	new_board = list(map(list, zip(*new_board)))
+	board_print(new_board)
+	return new_board
+
+	#primeiro transpomos para compactar verticalmente
+
+#Tipo board
+#O tipo tabuleiro representa o tabuleiro de um jogo de Same Game. A representação interna de um
+#jogo de Same Game é uma lista de listas, em que as sublistas correspondem às linhas do tabuleiro do
+#jogo. Uma sublista tem uma representação do conteúdo da linha. O conteúdo de uma posição é
+#representado pelo tipo color. O canto superior esquerdo corresponde à posição (0,0). O canto
+#inferior direito corresponde à posição (<nºlinhas>-1,<nºcolunas>-1).
+
+def board_width(board):
+	return len(board[0])
+def board_height(board):
+	return len(board)
+def board_get_color(board, pos):
+	return board[pos_l(pos)][pos_c(pos)]
+def board_del_color(board, pos):
+	board[pos_l(pos)][pos_c(pos)] = 0
+def board_print(board):
+	utils.print_table(board, sep=' ')
